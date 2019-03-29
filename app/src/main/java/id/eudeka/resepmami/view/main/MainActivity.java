d2f91242a6561801fc3a2cd849bcabd5bb649bde
Muhammad Iqbal <chongg1243@gmail.com>
2019-03-29 23:02:22 +0700

    fix warning code in layout and set content layout to the right layout

--- app/src/main/java/id/eudeka/resepmami/view/main/MainActivity.java
+++ app/src/main/java/id/eudeka/resepmami/view/main/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity implements MainNavigator {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_list_recipe);
 
         SampleViewModel sampleViewModel = new SampleViewModel(Injection.provideSampleRepository(this));
         sampleViewModel.setMainNavigator(this);
